b8ac943385be90feb7856cce3fa750cc64d9b686
Chetan51423 <chetanshinde51423@gamil.com>
2024-03-02 22:12:26 +0530

    Steup the server, send diffrent response according to different requests, get queries from the url diaplay it in response

--- Index.js
+++ Index.js
@@ -1,1 +1,43 @@
-console.log("hello chetan here!")
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+
+const server = http.createServer((req, res)=>{
+    const log = `${Date.now()}:${req.method} ${req.url} New request received\n`
+    const myurl = url.parse(req.url, true)
+    
+    if(req.url!=="/favicon.ico")
+    {
+        console.log(myurl)
+        fs.appendFile("./log.txt", log, (err, data)=>{
+            if(err)
+            {
+                console.log(err);
+            }
+            else
+            {
+                
+                switch(myurl.pathname)
+                {
+                    case "/":
+                        const username = myurl.query.name
+                        res.end(`Hello ${username} this is HomePage`)
+                        break
+                    case "/contact":
+                        res.end("contact page")
+                        break
+                    case "/about":
+                        res.end("about page")
+                        break
+                    default:
+                        res.end("404 Not Found")
+                }
+            }
+        })
+    }
+    
+})
+
+server.listen(8000, ()=>{
+    console.log("server is running successfully")
+})
